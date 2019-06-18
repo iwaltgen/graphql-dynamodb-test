79846044d610bb45c18221c58634dccd66c2181f
iwaltgen <iwaltgen@gmail.com>
2019-06-18 23:32:45 +0900

    feat: 파라메터 사용하는 Query 추가

--- handler.ts
+++ handler.ts
@@ -3,13 +3,21 @@ import { ApolloServer, gql } from 'apollo-server-lambda';
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    say(name: String!): String!
   }
 `;
 
 const resolvers = {
   Query : {
     hello: () => 'Hello world!',
+    say: (obj: any, args: any, context: any, info: any) => {
+      console.log("obj: ", obj);
+      console.log("args: ", args);
+      console.log("context: ", context);
+      console.log("info: ", info);
+      return `${args.name} said.`;
+    },
   }
 };
 
