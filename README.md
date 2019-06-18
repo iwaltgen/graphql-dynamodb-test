9900b4b38be76eb64642585274384462ad49861a
iwaltgen <iwaltgen@gmail.com>
2019-06-18 19:38:54 +0900

    feat: 기본 초기 설정

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# graphql-dynamodb-test
---
+++ handler.ts
@@ -0,0 +1,32 @@
+
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query : {
+    hello: () => 'Hello world!',
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+});
+
+exports.graphql = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
